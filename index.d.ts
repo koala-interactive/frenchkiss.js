3b1706ec4461e63ccc1a38dc3d72c193791027df
= <vthibault.mobile@gmail.com>
2019-04-12 11:07:49 +0200

    ✨  Introduce typescript definition

---
+++ index.d.ts
@@ -0,0 +1,47 @@
+declare module 'frenchkiss' {
+  type pluralRule = (count: number) => string;
+
+  type missingVariableHandler = (
+    variable: string,
+    key: string,
+    language: string
+  ) => string;
+
+  interface StoreData {
+    [key: string]: string | number;
+  }
+
+  interface CacheData {
+    [key: string]: (
+      params?: object,
+      pluralRule?: pluralRule,
+      key?: string,
+      language?: string,
+      missingVariableHandler?: missingVariableHandler
+    ) => string;
+  }
+
+  interface StoreItems {
+    [key: string]: StoreData;
+  }
+
+  interface CacheItems {
+    [key: string]: CacheData;
+  }
+
+  export const cache: CacheItems;
+  export const store: StoreItems;
+  export function t(key: string, params?: object, language?: string): string;
+  export function onMissingKey(
+    missingKeyHandler: (key: string) => string
+  ): void;
+  export function onMissingVariable(
+    missingVariableHandler: missingVariableHandler
+  ): void;
+  export function locale(language?: string): string;
+  export function fallback(language: string): string;
+  export function unset(language: string): void;
+  export function set(language: string, table: StoreData): void;
+  export function extend(language: string, table: StoreData): void;
+  export function plural(language: string, pluralRule: pluralRule): void;
+}
